77fc087ddf816341d40964a6ca36cc660ca46497
Serhii Manko <msa01051984@gmail.com>
2024-08-10 23:58:24 +0300

    Desktop

---
+++ script.js
@@ -0,0 +1,6 @@
+const slider = document.querySelector(".customer-list");
+const slides = slider.querySelectorAll(".customer-item");
+const prevSlide = document.getElementById("prev");
+const nextSlide = document.getElementById("next");
+
+let currentIndex = 0;
