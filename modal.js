9fb65854d75286fa7164386ae14610e3eda2213e
Serhii Manko <msa01051984@gmail.com>
2024-08-12 21:40:59 +0300

    registerform

--- modal.js
+++ modal.js
@@ -15,20 +15,56 @@ function closeModal() {
 
 function onSubmitForm(event) {
   event.preventDefault();
+
+  const submitButton = event.target.querySelector('button[type="submit"]');
+  submitButton.disabled = true;
+  submitButton.innerText = "Відправка...";
+
   const firstName = document.getElementById("firstName").value;
   const lastName = document.getElementById("lastName").value;
   const email = document.getElementById("email").value;
   const phone = document.getElementById("phone").value;
 
-  console.log("Форма відправлена:", { firstName, lastName, email, phone });
+  //   console.log("Форма відправлена:", { firstName, lastName, email, phone });
 
-  if (!firstName || !lastName || !email || !phone) {
-    alert("Будь-ласка, заповніть всі поля. Дякую");
+  const message = `Новий реєстраційний запит:\n
+    firstName = ${firstName}\n
+    lastName = ${lastName}\n
+    email = ${email}\n
+    phone = ${phone}\n`;
 
-  } else {
-    alert("Форма відправлена успішно!");
-    closeModal()
-  }
+  const token = "7293319206:AAGYdVOrYehed-LR4vsr4CuqLcsHH3ZDzhc";
+  const chatID = "-1002167205144";
+  const url = `https://api.telegram.org/bot${token}/sendMessage`;
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      chat_id: chatID,
+      text: message,
+    }),
+  })
+    .then((response) => {
+      response;
+      if (response.ok) {
+        alert("Форма відправлена успішно!");
+        closeModal();
+        registrationForm.reset();
+      } else {
+        throw new Error("Помилка сервера: " + response.statusText);
+      }
+    })
+    .catch((error) => {
+      alert("Помилка, спробуйте ще раз!");
+      console.log("Error: ", error);
+    })
+    .finally(() => {
+      submitButton.disabled = false;
+      submitButton.innerText = "Registration";
+    });
 }
 
 registrationForm.addEventListener("submit", onSubmitForm);
